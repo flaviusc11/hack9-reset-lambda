2e3c390bb0f7c2b1a11780d10c728483de0eadd2
Flavius Condrea <flavius.condrea@yahoo.com>
2019-12-14 05:57:42 +0200

    Return 201 always

--- src/main/scala/LambdaHandler.scala
+++ src/main/scala/LambdaHandler.scala
@@ -18,74 +18,9 @@ case class ErrorMessage(message: String) extends Response
 
 class LambdaHandler extends Proxy[String, Response] {
 
-  private val tables = List("flavius-test2")
-
-  private val _1: String = Option(System.getenv("AWS_ACCESS_KEY_ID")).getOrElse(throw new  Throwable("No AWS_ACCESS_KEY_ID defined!"))
-  private val _2: String = Option(System.getenv("AWS_SECRET_KEY")).getOrElse(throw new  Throwable("No AWS_SECRET_KEY defined!"))
-
-  val client: AmazonDynamoDB =
-    AmazonDynamoDBClient
-      .builder()
-      .build()
 
   override def handle(request: ProxyRequest[String], context: Context): Either[Throwable, ProxyResponse[Response]] = {
-    val tableDescriptions = getTableDescriptions
-    deleteTables(context)
-    createTables(tableDescriptions, context)
     Right(ProxyResponse(statusCode = 201, headers = Some(Map("Content-Type" -> "application/json")), body = Some(OK)))
-  }
-
-  private def getTableDescriptions: Seq[TableDescription] = {
-    try {
-      tables.map(client.describeTable).map(_.getTable)
-    } catch {
-      case _: Throwable =>
-        List()
-    }
-  }
-
-  private def deleteTables(context: Context): Unit = {
-    try {
-      context.getLogger.log(s"Initiate the process of deleting tables $tables"+ lineSeparator())
-      tables.map(client.deleteTable)
-
-      while(getTableDescriptions.nonEmpty) {
-        context.getLogger.log("Still waiting to delete some tables..."+lineSeparator())
-        Thread.sleep(200)
-      }
-      context.getLogger.log(s"All tables were deleted with success"+lineSeparator())
-    } catch {
-      case t: Throwable =>
-        context.getLogger.log(s"Some tables could not be deleted $t"+lineSeparator())
-    }
-  }
-
-  case class TableDesc(tableName: String, readCapacity: Long, writeCapacity: Long)
-
-  private def createTables(tableDescriptions: Seq[TableDescription], context: Context) = {
-    try {
-      context.getLogger.log(lineSeparator())
-      context.getLogger.log(s"Creating tables $tableDescriptions"+lineSeparator()+lineSeparator())
-
-      tableDescriptions
-        .map { td =>
-          context.getLogger.log(s"$td"+lineSeparator())
-          new CreateTableRequest(td.getTableName, td.getKeySchema)
-            .withAttributeDefinitions(td.getAttributeDefinitions)
-            .withBillingMode("PAY_PER_REQUEST")
-            .withLocalSecondaryIndexes(td.getLocalSecondaryIndexes.asInstanceOf[util.Collection[LocalSecondaryIndex]])
-            .withGlobalSecondaryIndexes(td.getGlobalSecondaryIndexes.asInstanceOf[util.Collection[GlobalSecondaryIndex]])
-        }.map(client.createTable)
-
-      while(getTableDescriptions.size < tables.size) {
-        context.getLogger.log("Still waiting to create some tables..."+lineSeparator())
-        Thread.sleep(200)
-      }
-      context.getLogger.log("All tables were created with success"+lineSeparator())
-    } catch {
-      case t: Throwable =>
-        context.getLogger.log(s"The following exception was thrown while creating tables $tables: ${t.getMessage}"+lineSeparator())
-    }
   }
 
 }
